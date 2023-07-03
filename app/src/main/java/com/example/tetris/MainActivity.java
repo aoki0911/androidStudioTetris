fbd9c00dc90fb7e178087ddae1621f01f8c4a30a
aoki0911 <e6a21111c9@edu.teu.ac.jp>
2023-07-03 11:28:14 +0900

    blocksクラスの分割

--- app/src/main/java/com/example/tetris/MainActivity.java
+++ app/src/main/java/com/example/tetris/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.tetris;
 
+import static com.example.tetris.blocks.iBlock;
+import static com.example.tetris.blocks.jBlock;
+import static com.example.tetris.blocks.lBlock;
+import static com.example.tetris.blocks.oBlock;
+import static com.example.tetris.blocks.sBlock;
+import static com.example.tetris.blocks.tBlock;
+import static com.example.tetris.blocks.zBlock;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -37,25 +45,19 @@ public class MainActivity extends AppCompatActivity {
     int offsety=0;
 
     int[][] field=new int[15][10];
-    public static final int tBlock = 1;
-    public static final int sBlock = 2;
-    public static final int iBlock = 3;
-    public static final int oBlock = 4;
-    public static final int lBlock = 5;
-    public static final int jBlock = 6;
-    public static final int zBlock = 7;
+
 
     int motion=Stational;
     public static final int Stational=0;
     public static final int Left=1;
     public static final int Right=2;
 
-    int num=0;
+
 
     public static final int blockLenght=4;
-    int[][] nowBlock=new int[blockLenght][blockLenght];
 
-    boolean moveflag;
+
+    static boolean moveflag;
 
 
     @Override
@@ -147,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
                 //ブロック出現処置
                 for(int i=0;i<blockLenght;i++) {
                     for (int j = 0; j < blockLenght; j++) {
-                        if (nowBlock[i][j] == 1) {
+                        if (blocks.nowBlock[i][j] == 1) {
                             drawMoveBlock(+offsetx + j, offsety + i, ca);
                         }
                     }
@@ -165,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
                 int px=x*blocksize;
                 int py=y*blocksize;
 
-                switch (num){
+                switch (blocks.num){
                     case tBlock:
                         paint.setColor(Color.BLUE);
                         ca.drawRect(px+blocksize,py+blocksize,px,py,paint);
@@ -244,81 +246,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    class blocks{
 
-        public void blocks(){
-
-            //ブロックランダム抽選
-            num=(int)(Math.random()*7)+1;
-
-            switch (num){
-                case tBlock:
-                    nowBlock=new int[][]{
-                            {0, 0, 0, 0},
-                            {0, 0, 1, 0},
-                            {0, 1, 1, 1},
-                            {0, 0, 0, 0}
-                    };
-                    break;
-
-                case sBlock:
-                    nowBlock=new int[][]{
-                            {0, 0, 0, 0},
-                            {0, 1, 1, 0},
-                            {1, 1, 0, 0},
-                            {0, 0, 0, 0}
-                    };
-                    break;
-
-                case iBlock:
-                    nowBlock=new int[][]{
-                            {0, 1, 0, 0},
-                            {0, 1, 0, 0},
-                            {0, 1, 0, 0},
-                            {0, 1, 0, 0}
-                    };
-                    break;
-
-                case oBlock:
-                    nowBlock=new int[][]{
-                            {0, 1, 1, 0},
-                            {0, 1, 1, 0},
-                            {0, 0, 0, 0},
-                            {0, 0, 0, 0}
-                    };
-                    break;
-
-                case lBlock:
-                    nowBlock=new int[][]{
-                            {0, 0, 0, 0},
-                            {0, 1, 1, 1},
-                            {0, 1, 0, 0},
-                            {0, 0, 0, 0}
-                    };
-                    break;
-
-                case jBlock:
-                    nowBlock=new int[][]{
-                            {0, 1, 0, 0},
-                            {0, 1, 1, 1},
-                            {0, 0, 0, 0},
-                            {0, 0, 0, 0}
-                    };
-                    break;
-                case zBlock:
-                    nowBlock=new int[][]{
-                            {0, 0, 0, 0},
-                            {0, 1, 1, 0},
-                            {0, 0, 1, 1},
-                            {0, 0, 0, 0}
-                    };
-                    break;
-                default:
-                    break;
-            };
-            moveflag=true;
-        }
-    }
 
     class Reset{
         public void initstartpoi(){
