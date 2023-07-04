5616b733a32c6bad3c778e677c503371f8c84c5b
aoki0911 <e6a21111c9@edu.teu.ac.jp>
2023-07-04 14:28:35 +0900

    90°ずつ回転したブロックの定義

--- app/src/main/java/com/example/tetris/blocks.java
+++ app/src/main/java/com/example/tetris/blocks.java
@@ -5,6 +5,7 @@ import static com.example.tetris.Draw.blockLenght;
 
 public class blocks {
     static int num = 0;
+    static int rotetanum = 0;
     public static final int tBlock = 1;
     public static final int sBlock = 2;
     public static final int iBlock = 3;
@@ -88,4 +89,203 @@ public class blocks {
         ;
         MainActivity.moveflag = true;
     }
-}
+
+    public void roteta() {
+        switch (num) {
+            case tBlock:
+                switch (rotetanum) {
+                    case 0:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 0, 1, 0},
+                                {0, 1, 1, 1},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 1:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 0, 1, 0},
+                                {0, 0, 1, 1},
+                                {0, 0, 1, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 2:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 0, 0, 0},
+                                {0, 1, 1, 1},
+                                {0, 0, 1, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 3:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 0, 1, 0},
+                                {0, 1, 1, 0},
+                                {0, 0, 1, 0}
+                        };
+                        rotetanum = 0;
+                        break;
+                }
+                break;
+            case sBlock:
+                switch (rotetanum) {
+                    case 0:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 1, 1, 0},
+                                {1, 1, 0, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 1:
+                        nowBlock = new int[][]{
+                                {0, 1, 0, 0},
+                                {0, 1, 1, 0},
+                                {0, 0, 1, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum = 0;
+                        break;
+                }
+                break;
+            case iBlock:
+                switch (rotetanum) {
+                    case 0:
+                        nowBlock = new int[][]{
+                                {0, 1, 0, 0},
+                                {0, 1, 0, 0},
+                                {0, 1, 0, 0},
+                                {0, 1, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 1:
+                        nowBlock = new int[][]{
+                                {1, 1, 1, 1},
+                                {0, 0, 0, 0},
+                                {0, 0, 0, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum = 0;
+                        break;
+                }
+                break;
+            case oBlock:
+                nowBlock = new int[][]{
+                        {0, 1, 1, 0},
+                        {0, 1, 1, 0},
+                        {0, 0, 0, 0},
+                        {0, 0, 0, 0}
+                };
+                break;
+            case lBlock:
+                switch (rotetanum) {
+                    case 0:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 1, 1, 1},
+                                {0, 1, 0, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 1:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 1, 1, 0},
+                                {0, 0, 1, 0},
+                                {0, 0, 1, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 2:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 0, 1, 0},
+                                {1, 1, 1, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 3:
+                        nowBlock = new int[][]{
+                                {0, 1, 0, 0},
+                                {0, 1, 0, 0},
+                                {0, 1, 1, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum = 0;
+                        break;
+                }
+                break;
+            case jBlock:
+                switch (rotetanum) {
+                    case 0:
+                        nowBlock = new int[][]{
+                                {0, 1, 0, 0},
+                                {0, 1, 1, 1},
+                                {0, 0, 0, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 1:
+                        nowBlock = new int[][]{
+                                {0, 1, 1, 0},
+                                {0, 1, 0, 0},
+                                {0, 1, 0, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 2:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {1, 1, 1, 0},
+                                {0, 0, 1, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 3:
+                        nowBlock = new int[][]{
+                                {0, 0, 1, 0},
+                                {0, 0, 1, 0},
+                                {0, 1, 1, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum = 0;
+                        break;
+                }
+                break;
+            case zBlock:
+                switch (rotetanum) {
+                    case 0:
+                        nowBlock = new int[][]{
+                                {0, 0, 0, 0},
+                                {0, 1, 1, 0},
+                                {0, 0, 1, 1},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum++;
+                        break;
+                    case 1:
+                        nowBlock = new int[][]{
+                                {0, 0, 1, 0},
+                                {0, 1, 1, 0},
+                                {0, 1, 0, 0},
+                                {0, 0, 0, 0}
+                        };
+                        rotetanum = 0;
+                        break;
+                }
+                break;
+            }
+        }
+    }
