863c67b224c8566270d7267407002f5967983e33
aoki0911 <e6a21111c9@edu.teu.ac.jp>
2023-07-12 10:20:17 +0900

    音楽再生メソッド、音楽再生停止メソッドの作成

--- app/src/main/java/com/example/tetris/titleView.java
+++ app/src/main/java/com/example/tetris/titleView.java
@@ -1,6 +1,7 @@
 package com.example.tetris;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -8,6 +9,8 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class titleView extends AppCompatActivity {
+    MediaPlayer mp;
+
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -15,14 +18,31 @@ public class titleView extends AppCompatActivity {
 
 
         ImageButton titleButton = findViewById(R.id.titleButton);
+        mp = MediaPlayer.create(this, R.raw.ongaku1);
         titleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MainActivity.startFlag = true;
                 Intent intent = new Intent(titleView.this, MainActivity.class);
                 startActivity(intent);
+                musicPlay();
                 finish();
             }
         });
+    }
+
+    public void musicPlay() {
+        if (mp != null && !mp.isPlaying()) {
+            mp.start();
+        }
+    }
+
+    public void musicStop() {
+        if (mp != null) {
+            mp.stop();
+            mp.reset();
+            mp.release();
+            mp = null;
+        }
     }
 }
